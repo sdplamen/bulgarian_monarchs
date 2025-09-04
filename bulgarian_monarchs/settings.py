8e4e852c98bc88ba77f05bb15af6c1c91cdcf64d
Plamen Stoilkov <sdplamen@gmail.com>
2025-09-04 14:36:25 +0300

    Install whitenoise

--- bulgarian_monarchs/settings.py
+++ bulgarian_monarchs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l6-6(n_!^))t)n%=fi=2!uxzrx0%c=xy1sf_%c-*6)+7y_j9j^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','bulgarian-monarchs.onrender.com']
 
