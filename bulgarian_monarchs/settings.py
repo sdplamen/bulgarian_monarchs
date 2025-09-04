5fd97314940a9c0950b489f196e5bbcda5de774b
Plamen Stoilkov <sdplamen@gmail.com>
2025-09-04 14:20:49 +0300

    Prepare to deploy

--- bulgarian_monarchs/settings.py
+++ bulgarian_monarchs/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'bulgarian_monarchs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'rulers_db',
     }
 }
 
