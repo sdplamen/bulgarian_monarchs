778560f90ec004886e6233452a7a7c75c7e96a3b
Plamen Stoilkov <sdplamen@gmail.com>
2025-09-04 13:17:46 +0300

    Prepare to deploy

--- bulgarian_monarches/settings.py
+++ bulgarian_monarches/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-l6-6(n_!^))t)n%=fi=2!uxzrx0%c=xy1sf_%c-*6)+7y_j9j^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['127.0.0.1','localhost','bulgarian-monarchs.onrender.com']
 
 
 # Application definition
